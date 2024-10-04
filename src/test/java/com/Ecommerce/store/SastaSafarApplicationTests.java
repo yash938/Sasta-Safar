6e361b301430b374b9926850785fbc690e88d9ca
Yash Sharma <yash32860@gmail.com>
2024-10-04 15:00:32 +0530

    this is my first commit

---
+++ src/test/java/com/Ecommerce/store/SastaSafarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ecommerce.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SastaSafarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
