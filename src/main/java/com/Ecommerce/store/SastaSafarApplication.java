2ba92f7b200564fc8e3388a470b972092b05e4fa
Yash Sharma <yash32860@gmail.com>
2024-10-04 15:00:32 +0530

    this is my first commit

---
+++ src/main/java/com/Ecommerce/store/SastaSafarApplication.java
@@ -0,0 +1,13 @@
+package com.Ecommerce.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SastaSafarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SastaSafarApplication.class, args);
+	}
+
+}
