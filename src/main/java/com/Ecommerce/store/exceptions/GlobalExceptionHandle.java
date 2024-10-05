4a7cc9b5aab1785eef0ed7f93e20cbba2f3dca1c
Yash Sharma <yash32860@gmail.com>
2024-10-05 16:54:15 +0530

    hadling API Exception code is created

--- src/main/java/com/Ecommerce/store/exceptions/GlobalExceptionHandle.java
+++ src/main/java/com/Ecommerce/store/exceptions/GlobalExceptionHandle.java
@@ -5,8 +5,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandle {
@@ -23,4 +30,25 @@ public class GlobalExceptionHandle {
 
     }
 
+
+
+
+    //handle api exception handling
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,Object>> handleApiReponseException(MethodArgumentNotValidException ex){
+
+        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+
+        Map<String, Object> objectObjectHashMap = new HashMap<>();
+
+        allErrors.stream().forEach(objectError -> {
+
+            String defaultMessage = objectError.getDefaultMessage();
+            String field = ((FieldError) objectError).getField();
+
+            objectObjectHashMap.put(field,defaultMessage);
+
+        });
+        return new ResponseEntity<>(objectObjectHashMap,HttpStatus.BAD_REQUEST);
+    }
 }
