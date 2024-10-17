6ad476178ef490b4f84c3fc2ee15d4cd56a18d63
Yash Sharma <yash32860@gmail.com>
2024-10-17 23:01:45 +0530

    Order and OrderItem entity is created

---
+++ src/main/java/com/Ecommerce/store/entities/OrderItems.java
@@ -0,0 +1,29 @@
+package com.Ecommerce.store.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItems {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int orderItemId;
+
+    private int quantity;
+    private int totalPrice;
+
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+}
