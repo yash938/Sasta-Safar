67806fbb179b573dc14097dce147fafc014664de
Yash Sharma <yash32860@gmail.com>
2024-11-06 16:02:01 +0530

    some error has been resolved

--- src/test/java/com/Ecommerce/store/SastaSafarApplicationTests.java
+++ src/test/java/com/Ecommerce/store/SastaSafarApplicationTests.java
@@ -1,6 +1,6 @@
 package com.Ecommerce.store;
 
-import com.Ecommerce.store.Security.JwtHelper;
+
 import com.Ecommerce.store.entities.User;
 import com.Ecommerce.store.repository.UserRepo;
 import org.junit.jupiter.api.Test;
@@ -11,8 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SastaSafarApplicationTests {
 	@Autowired
 	private UserRepo userRepo;
-	@Autowired
-	private JwtHelper jwtHelper;
 
 	@Test
 	void contextLoads() {
