b32bb7462e8fad5155f6108801d3c818c0a0e40c
Yash Sharma <yash32860@gmail.com>
2024-11-14 11:05:09 +0530

    small changes in my code in docker

--- src/main/java/com/Ecommerce/store/repository/RoleRepo.java
+++ src/main/java/com/Ecommerce/store/repository/RoleRepo.java
@@ -2,9 +2,10 @@ package com.Ecommerce.store.repository;
 
 import com.Ecommerce.store.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface RoleRepo extends JpaRepository<Role,Integer> {
     Optional<Role>  findByName(String name);
 }
